09603f487e6fd928d5f16d0e7c14f6c42a82866c
Marburg1234 <172342315+Marburg1234@users.noreply.github.com>
2024-08-04 12:05:57 +0000

    before starting

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,11 +1,11 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to book_path(book)
+    # redirect_to book_path(book)
   end
 
   def destroy
@@ -13,10 +13,12 @@ class BookCommentsController < ApplicationController
       comment = BookComment.find(params[:id])
       if comment.user_id == current_user.id
         comment.destroy
-        redirect_to request.referer
-      else
-        redirect_to book_path(comment.book)
       end
+      @book = Book.find(params[:book_id])
+        # redirect_to request.referer
+      # else
+      #   redirect_to book_path(comment.book)
+
       # unless文で作成してものの読み込まれるが、削除も同時に実行される⇒redirect_toが2回読み込まれてエラーになる
       # unless comment.user_id == current_user.id
       #   redirect_to book_path(comment.book)
